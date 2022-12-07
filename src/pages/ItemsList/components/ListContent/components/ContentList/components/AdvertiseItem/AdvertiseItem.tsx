88fc97cebbd9f22a71ac48d4bd4f59d8a83e5f9d
Hoon <si932174@gmail.com>
2022-12-07 16:29:33 +0900

    refactor : AdvertiseItem 컴포넌트 TypeScript 적용

---
+++ src/pages/ItemsList/components/ListContent/components/ContentList/components/AdvertiseItem/AdvertiseItem.tsx
@@ -0,0 +1,11 @@
+function AdvertiseItem() {
+  return (
+    <div className="video">
+      <video muted autoPlay loop width="100%">
+        <source src="/videos/nikeRun33.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+}
+
+export default AdvertiseItem;
