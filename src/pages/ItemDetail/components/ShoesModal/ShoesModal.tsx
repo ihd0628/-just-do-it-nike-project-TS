581e69e27baa3aa647502275936396dba0a9c79d
Hoon <si932174@gmail.com>
2022-12-08 15:28:52 +0900

    refactor : ShoesModal 컴포넌트 TypeScript 적용

--- src/pages/ItemDetail/components/ShoesModal/ShoesModal.tsx
+++ src/pages/ItemDetail/components/ShoesModal/ShoesModal.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './ShoesModal.scss';
 import ShoesImgs from '../ShoesImgs/ShoesImgs';
-function ShoesModal({ closeShoesModal, shoesModal, imageUrl }) {
+
+interface PropsTypes {
+  closeShoesModal: () => void;
+  shoesModal: boolean;
+  imageUrl: string;
+}
+
+function ShoesModal({ closeShoesModal, shoesModal, imageUrl }: PropsTypes) {
   return (
     <div>
       {shoesModal && (
         <div className="shoesModal">
-          <div onClick={closeShoesModal} className="overlayWrap">
+          <div
+            role="presentation"
+            onClick={closeShoesModal}
+            className="overlayWrap"
+          >
             <ShoesImgs imageUrl={imageUrl} />
           </div>
         </div>
