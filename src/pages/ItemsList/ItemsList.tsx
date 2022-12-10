aeda82d772974c7350ebb98868b42f71b990da4e
Hoon <si932174@gmail.com>
2022-12-10 10:29:10 +0900

    refactor : ItemList 필터 조건 쿼리스트링 구하는 로직 분리

--- src/pages/ItemsList/ItemsList.tsx
+++ src/pages/ItemsList/ItemsList.tsx
@@ -8,6 +8,49 @@ import standardObject from './components/FilterBar/constantData/standardObject';
 import './itemList.scss';
 import { CheckList, ProductTypes } from './ItemListTypes';
 
+class GetFilterString {
+  private urlForSubmit: string;
+
+  constructor(
+    selectedSizeInput: Array<string>,
+    selectedColorInput: Array<string>,
+    checkListInput: CheckList
+  ) {
+    let quertString = '';
+    quertString += GetFilterString.selectSizeGetterForUrl(selectedSizeInput);
+    quertString += GetFilterString.selectColorGetterForUrl(selectedColorInput);
+    quertString += GetFilterString.checkListGetterForUrl(checkListInput);
+    this.urlForSubmit = quertString;
+  }
+
+  static selectSizeGetterForUrl(selectedSizeInput: Array<string>) {
+    let selectSizeForUrl = '';
+    selectedSizeInput.forEach(size => {
+      selectSizeForUrl += `size=${size}&`;
+    });
+    return selectSizeForUrl;
+  }
+
+  static selectColorGetterForUrl(selectedColorInput: Array<string>) {
+    let selectColorForUrl = '';
+    selectedColorInput.forEach(color => {
+      selectColorForUrl += `color=${color}&`;
+    });
+    return selectColorForUrl;
+  }
+
+  static checkListGetterForUrl(checkListInput: CheckList) {
+    let checkListForUrl = '';
+    const checkListNames = Object.keys(checkListInput);
+    checkListNames.forEach(checkListName => {
+      checkListInput[checkListName].forEach(checkedList => {
+        checkListForUrl += `${checkListName}=${checkedList}&`;
+      });
+    });
+    return checkListForUrl;
+  }
+}
+
 function ItemList() {
   const [products, setProducts] = useState<Array<ProductTypes>>([]);
   const [sortStandard, setSortStandard] = useState<string>('신상품순');
@@ -18,43 +61,19 @@ function ItemList() {
   const [offset, setOffset] = useState<number>(0);
   const [limit, setLimit] = useState<number>(5);
   const [, setSearchParams] = useSearchParams();
+  // const location = useLocation();
 
   const itemListCount = useRef<HTMLDivElement>(null);
 
-  const selectSizeGetterForUrl = (selectedSizeInput: Array<string>) => {
-    let selectSizeForUrl = '';
-    selectedSizeInput.forEach(size => {
-      selectSizeForUrl += `size=${size}&`;
-    });
-    return selectSizeForUrl;
-  };
-
-  const selectColorGetterForUrl = (selectedColorInput: Array<string>) => {
-    let selectColorForUrl = '';
-    selectedColorInput.forEach(color => {
-      selectColorForUrl += `color=${color}&`;
-    });
-    return selectColorForUrl;
-  };
-
-  const checkListGetterForUrl = (checkListInput: CheckList) => {
-    let checkListForUrl = '';
-    const checkListNames = Object.keys(checkListInput);
-    checkListNames.forEach(checkListName => {
-      checkListInput[checkListName].forEach(checkedList => {
-        checkListForUrl += `${checkListName}=${checkedList}&`;
-      });
-    });
-    return checkListForUrl;
-  };
-
   useEffect(() => {
     const sortStandardForSubmit = standardObject[sortStandard];
-    let urlForSubmit = `offset=${offset}&limit=${limit}&sort=${sortStandardForSubmit}&`;
-
-    urlForSubmit += selectSizeGetterForUrl(selectedSize);
-    urlForSubmit += selectColorGetterForUrl(selectedColor);
-    urlForSubmit += checkListGetterForUrl(checkList);
+    const urlForSubmit = `offset=${offset}&limit=${limit}&sort=${sortStandardForSubmit}&`;
+    const queryStringForFilter = new GetFilterString(
+      selectedSize,
+      selectedColor,
+      checkList
+    );
+    console.log('queryStringForFilter : ', queryStringForFilter);
 
     console.log('urlForSubmit : ', urlForSubmit);
 
